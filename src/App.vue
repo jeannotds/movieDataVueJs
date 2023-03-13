a8a318d39001f8e4df612d60f92e0689c798fba1
jeannot diambu <diambujeannot@gmail.com>
2023-03-13 11:21:14 +0100

    feat : add structure and style to home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,50 @@
 <template>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1>
+        <span>Data</span>
+        Story
+      </h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  /* background-color: #42b883; */
+}
+
+body{
+  background-color: #34595E;
+}
+
+a{
+  text-decoration: none;
+}
+
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: 0px 0px 6px rgba(0, 0, 0, 0.1);
+}
+
+h1{
+  color: white;
+  font-size: 17px;
+  font-size: 28px;
+}
+
+span{
+  color: #42b883;
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="../assets/story.jpg" class="feature-img" />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            dolorum obcaecati neque iure unde deleniti quos sit laborum culpa
+            veniam repellendus labore consequatur placeat eligendi dignissimos
+            aliquid accusantium, illum accusamus.
+          </p>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +23,40 @@ export default {
   components: {},
 };
 </script>
+
+<style>
+.feature-card {
+  position: relative;
+}
+.feature-img {
+  display: block;
+  width: 100%;
+  height: 300px;
+  opacity: 0.4;
+  object-fit: cover;
+  position: relative;
+  z-index: 0;
+}
+
+.detail {
+  position: absolute;
+  /* top: 0; */
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 16px;
+  z-index: 1;
+}
+
+h3{
+  color: rgba(255, 255, 255, 0.67);
+  margin-bottom: 16px;
+}
+
+p{
+  color: rgba(255, 255, 255, 0.67);
+  margin-bottom: 16px;
+}
+
+</style>
